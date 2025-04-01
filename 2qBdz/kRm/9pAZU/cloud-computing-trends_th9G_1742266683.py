a3f85861726acd8d94abe74b342f2c61a47f655c
lucaJakA717014rI <bandbox_drawls_0h@icloud.com>
2025-04-01 01:16:14 +0100

    Update the file's inline documentation to reflect recent changes

--- 2qBdz/kRm/9pAZU/cloud-computing-trends_th9G_1742266683.py
+++ 2qBdz/kRm/9pAZU/cloud-computing-trends_th9G_1742266683.py
@@ -1,7 +1,6 @@
 import pyodbc
 # Define the connection string
 conn = pyodbc.connect(
-    "DRIVER={SQL Server};"
     "SERVER=localhost\\SQLEXPRESS;"  # Adjust based on your setup
     "DATABASE=StockMarket;"
     "Trusted_Connection=yes;"
